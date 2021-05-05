1aa9aad3a78e9304e1e5b1ea1268da5f7e5ddac6
Giorgio Giannotta <jorge_upa@msn.com>
2021-05-05 16:21:46 +0200

    Finish the Core of the game

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,9 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    let choicesArray = ["👊","🖐","✌️"]
+    @State var computerChoice = "✌️"
+    @State var result = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .center, spacing: 0, content: {
+            Text("Rock Paper Scissors")
+                .font(.title)
+                .padding(.bottom,150)
+            Text("Computer Choice")
+                .font(.title3)
+                .padding(.bottom,50)
+            Text(computerChoice)
+                .font(.largeTitle)
+                .padding(.bottom,50)
+            
+            Divider()
+            
+            HStack(alignment: .center, spacing: 120, content: {
+                Button(action: {
+                    self.computerChoice = choicesArray.randomElement()!
+                    
+                    if computerChoice == "✌️"{
+                        result = "You Win!"
+                    }else if computerChoice == "🖐"{
+                        result = "You Lose"
+                    }else {
+                        result = "It's a Draw"
+                    }
+                }, label: {
+                    Text("👊")
+                        .font(.largeTitle)
+                })
+                Button(action: {
+                    self.computerChoice = choicesArray.randomElement()!
+                    
+                    if computerChoice == "👊"{
+                        result = "You Win!"
+                    }else if computerChoice == "✌️"{
+                        result = "You Lose"
+                    }else {
+                        result = "It's a Draw"
+                    }
+                }, label: {
+                    Text("🖐")
+                        .font(.largeTitle)
+                })
+                Button(action: {
+                    self.computerChoice = choicesArray.randomElement()!
+                    
+                    if computerChoice == "🖐"{
+                        result = "You Win!"
+                    }else if computerChoice == "👊"{
+                        result = "You Lose"
+                    }else {
+                        result = "It's a Draw"
+                    }
+                }, label: {
+                    Text("✌️")
+                        .font(.largeTitle)
+                })
+                
+            })
+            .padding(.top,50)
+            
+            Text("Your Choice")
+                .font(.title3)
+                .padding(.top,50)
+            Spacer()
+            Text(result)
+            Spacer()
+        })
     }
 }
 
